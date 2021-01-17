385eea64fa48d065afabd7c4e57763c4a66860fa
TovarasulTony <tonyfrum@yahoo.com>
2021-01-17 21:52:01 +0200

    update

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
-    s.listen()
+    s.listen(2)
     conn, addr = s.accept()
     with conn:
         print('Connected by', addr)
