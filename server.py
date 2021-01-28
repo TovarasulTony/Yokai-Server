6422daa2f6e284ae50eb5a3a05c350404ec42526
TovarasulTony <tonyfrum@yahoo.com>
2021-01-28 22:45:08 +0200

    update

--- server.py
+++ server.py
@@ -41,11 +41,11 @@ def clientthread(conn, addr, id):
         info_str+=";"
     info_str+=str(id)
     info_str+=","
-    info_str+=str(10*id)
+    info_str+="150"
     info_str+=","
-    info_str+="0"
+    info_str+="4"
     info_str+=","
-    info_str+="0"
+    info_str+=str(30+10*id)
     list_of_clients_info.append(info_str)
     print(info_str)
     populate_list_str=""
@@ -127,29 +127,29 @@ def broadcast(message, connection):
 from the list that was created at the beginning of  
 the program"""
 def remove(connection):
-    if connection in list_of_clients:  
-        list_of_clients.remove(connection)  
+    if connection in list_of_clients:
+        list_of_clients.remove(connection)
 
 id = 0
-while True:  
+while True:
     #global id 
     """Accepts a connection request and stores two parameters,  
     conn which is a socket object for that user, and addr  
     which contains the IP address of the client that just  
     connected"""
-    conn, addr = server.accept()  
+    conn, addr = server.accept()
   
     """Maintains a list of clients for ease of broadcasting  
     a message to all available people in the chatroom"""
-    list_of_clients.append(conn)  
+    list_of_clients.append(conn)
   
     # prints the address of the user that just connected  
-    print (addr[0] + " connected") 
+    print (addr[0] + " connected")
   
     # creates and individual thread for every user  
     # that connects
     id+=1
     start_new_thread(clientthread,(conn,addr, id))    
   
-conn.close()  
-server.close()  
+conn.close()
+server.close()
