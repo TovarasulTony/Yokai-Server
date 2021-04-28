f56feb17534638983a7faf78783ad5743dff431d
TovarasulTony <tonyfrum@yahoo.com>
2021-04-28 11:18:56 +0300

    update

--- player_holder.py
+++ player_holder.py
@@ -114,9 +114,9 @@ class PlayerHolder:
             print(new_position["z"])
             print(player_new_position["id"])
             #de mutat intr-o functie???
-            print(player_new_position["id"]["player_position"]["x"])
+            print(self.player_list["id"]["player_position"]["x"])
             self.player_list[player_new_position['id']]["player_position"]=new_position
-            print(player_new_position["id"]["player_position"]["x"])
+            print(self.player_list["id"]["player_position"]["x"])
             #--------------------
             '''
 
