4d123778214a5a0cb0f5f194e526bb4191c80906
TovarasulTony <tonyfrum@yahoo.com>
2021-05-13 20:14:53 +0300

    update

--- lobby.py
+++ lobby.py
@@ -32,24 +32,17 @@ class Lobby:
         start_new_thread(self.clientthread, (new_player,))
 
     def remove_potential_player(self, player):
-        '''
-        lobby_info = command
-        lobby_info["command_type"] = "LOBBY"
-        lobby_info["message"] = "load_next_lvl"
-        lobby_info["values"] = ""
-        self.send_message_to_player(player, lobby_info)
-        '''
         self.lobby_list.remove(player)
 
     def setup_lobby_player(self, player):
-        lobby_info = copy.deepcopy(command_dict)
-        lobby_info["command_type"] = "LOBBY"
-        lobby_info["message"] = "lobby_count"
-        lobby_info["values"] = len(self.lobby_list)
+        self.make_client_command(player, "lobby_count", len(self.lobby_list))
         #this line should be a function call
         self.send_message_to_player(player, lobby_info)
 
     def send_message_to_player(self, player, message_json):
+        string_message = "$"
+        string_message += json.dumps(message_json)
+        string_message += "$"
         print(player["connection"])
         print(player["connection"].send(bytes(json.dumps(message_json), 'UTF-8')))
 
@@ -61,16 +54,9 @@ class Lobby:
                 message_bulk = bytes_message.decode("utf-8")
                 message_list = message_bulk.split('$')
                 for message in message_list:
-                    #print(88888)
-                    #print(message)
                     if message == "":
                         """message may have no content if the connection  
                         is broken, in this case we remove the connection"""
-                        #print("cucu")  
-                        #print("MESAJ GOL")
-                        #player["connection"].close()
-                        #self.remove_potential_player(player["connection"])
-                        #self.remove(player["connection"])  
                         continue
                     message = json.loads(message)
                     terminate_thread_flag = self.execute_command(player, message)
@@ -88,3 +74,16 @@ class Lobby:
             self.command_callback(player, message)
             return True
         return False
+
+    def make_client_command(self, player, message, values=""):
+        command_json = copy.deepcopy(command_dict)
+        command_json["command_type"] = "LOBBY"
+        command_json["message"] = message
+        command_json["values"] = values
+        self.send_message_to_player(player, command_json)
+
+    def broadcast_command(self, player, message, values=""):
+        for player_in_list in self.player_list:
+            if player_in_list["id"] == player["id"]:
+                continue
+            self.make_client_command(player_in_list, message, values)
