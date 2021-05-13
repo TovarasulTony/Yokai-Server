c48fd2dda7abc41c35ccef844f1c4af872a39d10
TovarasulTony <tonyfrum@yahoo.com>
2021-05-13 20:19:24 +0300

    update

--- lobby.py
+++ lobby.py
@@ -87,3 +87,10 @@ class Lobby:
             if player_in_list["id"] == player["id"]:
                 continue
             self.make_client_command(player_in_list, message, values)
+            
+    def send_message_to_player(self, player, message_json):
+        string_message = "$"
+        string_message += json.dumps(message_json)
+        string_message += "$"
+        print(player["connection"])
+        print(player["connection"].send(bytes(string_message, 'UTF-8')))
