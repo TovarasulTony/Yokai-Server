e280cddc748622f10b64350039043bdc7cc195da
TovarasulTony <tonyfrum@yahoo.com>
2021-05-19 17:14:54 +0300

    update

--- player_holder.py
+++ player_holder.py
@@ -41,6 +41,8 @@ class PlayerHolder:
         self.inform_lobby_players_number()
         self.make_client_command(added_player, "set_primordial_id", added_player["player_info"]["id"])
         self.send_init_info(added_player)
+        print(111)
+        print(self.clue_handler.get_phantom_type())
         self.make_client_command(added_player, "set_phantom_type", self.clue_handler.get_phantom_type())
         self.broadcast_command(added_player, "add_new_player", json.dumps(added_player["player_info"]))
         print("player added: " + str(added_player["player_info"]["id"]))
