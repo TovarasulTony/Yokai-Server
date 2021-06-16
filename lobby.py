15a5e6895034affd6799570e900118fd9c1f945f
TovarasulTony <tonyfrum@yahoo.com>
2021-06-16 22:37:46 +0300

    update

--- lobby.py
+++ lobby.py
@@ -48,6 +48,7 @@ class Lobby:
                 bytes_message = player["connection"].recv(1024)
                 message_bulk = last_message
                 message_bulk += bytes_message.decode("utf-8")
+                print(message_bulk)
                 message_list = message_bulk.split('$')
                 if message_list[len(message_list) - 1] != "-" or message_list[len(message_list) - 1] != "--":
                     last_message = message_list[len(message_list) - 1]
